35a203e952c4a8e435105b382dcff22e6645f250
SebastianGarrido212 <sgarridogithub@gmail.com>
2026-01-13 14:23:57 -0300

    s

--- TrabajoEmpresa/settings.py
+++ TrabajoEmpresa/settings.py
@@ -138,6 +138,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 # --- CONFIGURACIÓN DE LA PAGINA ADMINISTRADOR ---
 
 JAZZMIN_SETTINGS = {
